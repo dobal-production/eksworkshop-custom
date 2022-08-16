fcf5b13a6f159da3b1b80181eb7e88df2ba67aa9
Dobal <dokeunoh@gmail.com>
2022-08-16 10:20:59 +0900

    Update 02.install-tools.sh

--- 02.install-tools.sh
+++ 02.install-tools.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~/environment
+
 # Install K8s Tools
 sudo curl --silent --location -o /usr/local/bin/kubectl \
    https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
