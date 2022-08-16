6f950ae9252e930d06eab774c984364232e8194d
Dobal <dokeunoh@gmail.com>
2022-08-16 10:28:13 +0900

    Update 02.install-tools.sh

--- 02.install-tools.sh
+++ 02.install-tools.sh
@@ -31,3 +31,12 @@ echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
 .  ~/.bash_profile
 
 cat ~/.bash_profile
+
+# install eks ctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
