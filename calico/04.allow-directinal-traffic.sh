d0aa8a40ca662c2a722e00f2504e0fd097ea886a
dokeun <dokeunoh@gmail.com>
2022-09-01 00:06:24 +0900

    add callico pull example

---
+++ calico/04.allow-directinal-traffic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cd ~/environment/calico_resources
+#wget https://eksworkshop.com/beginner/120_network-policies/calico/stars_policy_demo/directional_traffic.files/backend-policy.yaml
+#wget https://eksworkshop.com/beginner/120_network-policies/calico/stars_policy_demo/directional_traffic.files/frontend-policy.yaml
+
+cp ./backend-policy.yaml ~/environment/calico_resources/
+cp ./frontend-policy.yaml ~/environment/calico_resources/
