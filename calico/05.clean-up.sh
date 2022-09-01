754ca3e8592df4ec225d3c961f1332d37bab5968
dokeun <dokeunoh@gmail.com>
2022-09-01 13:39:12 +0900

    add script to remove calico daemonset

--- calico/05.clean-up.sh
+++ calico/05.clean-up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 kubectl delete namespace client stars management-ui
+kubectl delete deployments.apps -n kube-system calico-typha 
+kubectl delete deployments.apps -n kube-system calico-typha-horizontal-autoscaler 
 kubectl delete daemonset calico-node -n kube-system
