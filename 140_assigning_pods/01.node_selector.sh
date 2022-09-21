4aa5f3cb8f82d27a91bb87ef3cf121e69f19d8ee
dokeun <dokeunoh@gmail.com>
2022-09-21 22:36:38 +0900

    modify 140_assigning_pods https://www.eksworkshop.com/beginner/140_assigning_pods/

--- 140_assigning_pods/01.node_selector.sh
+++ 140_assigning_pods/01.node_selector.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# get all nodes
+echo "##### get all nodes"
 kubectl get nodes
 
-# 디스크 타입이 ssd인 노드만 가져오기
+echo "##### 디스크 타입이 ssd인 노드만 가져오기"
 kubectl get nodes --selector disktype=ssd
 
-# 첫 번째 노드에 disktype=ssd를 추가
+echo "##### 첫 번째 노드에 label disktype=ssd를 추가"
 export FIRST_NODE_NAME=$(kubectl get nodes -o json | jq -r '.items[0].metadata.name')
 kubectl label nodes ${FIRST_NODE_NAME} disktype=ssd
 kubectl get nodes --selector disktype=ssd
+sleep 10s
 
-# Deploy a nginx pod only to the node with the new label
-sleep 30s
-
+echo "##### Deploy a nginx pod only to the node with the new label"
 cat <<EoF > ~/environment/pod-nginx.yaml
 apiVersion: v1
 kind: Pod
@@ -32,5 +31,7 @@ EoF
 
 kubectl apply -f ~/environment/pod-nginx.yaml
 
+sleep 10s
+echo "##### Display node with disktype=ssd and pod in that"
 kubectl get pods -o wide
 kubectl get nodes --selector disktype=ssd
