60f60f1385b2be92d49bad594ea4218573400e18
dokeun <dokeunoh@gmail.com>
2022-09-21 23:29:19 +0900

    modify 160_advanced-networking https://www.eksworkshop.com/beginner/160_advanced-networking/

--- 160_advanced-networking/02.configure-cni.sh
+++ 160_advanced-networking/02.configure-cni.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# check cni version
+echo "##### check cni version\n"
 kubectl describe daemonset aws-node --namespace kube-system | grep Image | cut -d "/" -f 2
 
-# Configure custom networking
-echo "before configure"
+echo "##### # Configure custom networking"
+echo "##### before configure"
 kubectl describe daemonset aws-node -n kube-system | grep -A5 Environment
 kubectl set env ds aws-node -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true
-echo "after configure"
+
+echo "##### after configure"
 kubectl describe daemonset aws-node -n kube-system | grep -A5 Environment
 
-# Terminate all nodes
+echo "#####  Terminate all nodes"
 INSTANCE_IDS=(`aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --filters "Name=tag-key,Values=eks:cluster-name" "Name=tag-value,Values=eksworkshop*" --output text` )
 for i in "${INSTANCE_IDS[@]}"
 do
@@ -18,7 +19,7 @@ do
 	aws ec2 terminate-instances --instance-ids $i
 done
 
-# ops-view 다시 설치
+ops-view 다시 설치
 helm install kube-ops-view \
 stable/kube-ops-view \
 --set service.type=LoadBalancer \
