f0664fa6e93adcd21d0da13949ef4021674f7609
dokeun <dokeunoh@gmail.com>
2022-09-22 16:37:45 +0900

    modify 191_secrets

--- 191_secrets/01.create-a-secret.sh
+++ 191_secrets/01.create-a-secret.sh
@@ -2,16 +2,16 @@
 
 kubectl create ns secretslab
 
-# Create a text file containing your secret:
+echo "##### Create a text file containing your secret:"
 echo -n "am i safe?" > ./test-creds
 
-# Create your secret:
+echo "##### Create your secret:"
 kubectl create secret \
         generic test-creds \
         --from-file=test-creds=./test-creds \
         --namespace secretslab
 
-# Retrieve the secret via the CLI:
+echo "##### Retrieve the secret via the CLI:"
 kubectl get secret test-creds \
   -o jsonpath="{.data.test-creds}" \
   --namespace secretslab | \
