00a55addc89ada2cfd60dab2618ebd4bee471a38
dokeun <dokeunoh@gmail.com>
2022-09-25 21:02:50 +0900

    add 240_monitoring
    https://www.eksworkshop.com/intermediate/240_monitoring/

---
+++ 240_monitoring/01.deploy-prometheus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# add prometheus Helm repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+# add grafana Helm repo
+helm repo add grafana https://grafana.github.io/helm-charts
+
+kubectl create namespace prometheus
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm install prometheus prometheus-community/prometheus \
+    --namespace prometheus \
+    --set alertmanager.persistentVolume.storageClass="gp2" \
+    --set server.persistentVolume.storageClass="gp2"
+
+sleep 2m
+
+kubectl get all -n prometheus
