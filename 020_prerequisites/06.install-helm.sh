d05ffd6c82a6405f998114bc13bb0f2d62ca594d
dokeun <dokeunoh@gmail.com>
2023-02-08 00:01:13 +0900

    add kube-ops-view

--- 020_prerequisites/06.install-helm.sh
+++ 020_prerequisites/06.install-helm.sh
@@ -13,16 +13,6 @@ helm completion bash >> ~/.bash_completion
 . ~/.bash_completion
 source <(helm completion bash)
 
-# ops_view
-cd ~/environment
-git clone https://codeberg.org/hjacobs/kube-ops-view.git
-cd kube-ops-view
-sed -i 's/ClusterIP/LoadBalancer/g' deploy/service.yaml 
-kubectl apply -k deploy
-
-# sleep 20s
-kubectl get svc kube-ops-view | tail -n 1 | awk '{ print "Kube-ops-view URL = http://"$4 }'
-
 # echo "##### Deploy the metric server"
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
 
---
+++ 020_prerequisites/07.kube-ops-view.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/environment
+git clone https://codeberg.org/hjacobs/kube-ops-view.git
+cd kube-ops-view
+sed -i 's/ClusterIP/LoadBalancer/g' deploy/service.yaml 
+kubectl apply -k deploy
+
+sleep 20s
+kubectl get svc kube-ops-view | tail -n 1 | awk '{ print "Kube-ops-view URL = http://"$4 }'
