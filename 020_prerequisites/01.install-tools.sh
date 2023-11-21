308bce988b1f9a17be2c06ec3c1423fb77f4bd6e
Dobal <dokeunoh@gmail.com>
2023-11-21 17:19:29 +0900

    update lb version

--- 020_prerequisites/01.install-tools.sh
+++ 020_prerequisites/01.install-tools.sh
@@ -26,7 +26,7 @@ kubectl completion bash >>  ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
-echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
+echo 'export LBC_VERSION="v2.5.4"' >>  ~/.bash_profile
 echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
 .  ~/.bash_profile
 
