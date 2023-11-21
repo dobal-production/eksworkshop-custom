fc598e0c76b65f301b5db415a1ce43f79eed7936
Dobal <dokeunoh@gmail.com>
2023-11-21 20:52:54 +0900

    remove image.tag in lb controller

--- 130_exposing-service/04.ingress_controller_alb.sh
+++ 130_exposing-service/04.ingress_controller_alb.sh
@@ -41,9 +41,7 @@ helm upgrade -i aws-load-balancer-controller \
     -n kube-system \
     --set clusterName=eksworkshop-eksctl \
     --set serviceAccount.create=false \
-    --set serviceAccount.name=aws-load-balancer-controller \
-    --set image.tag="${LBC_VERSION}" \
-    --version="${LBC_CHART_VERSION}"
+    --set serviceAccount.name=aws-load-balancer-controller 
 
 kubectl -n kube-system rollout status deployment aws-load-balancer-controller
 
