6bf7a573aabe927e3f155bf6b98185223602df56
Dobal <dokeunoh@gmail.com>
2023-12-05 17:27:50 +0900

    add kube-ops-view installation by service type

---
+++ 020_prerequisites/08.kube-ops-view-nlb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd ~/environment
+git clone https://codeberg.org/hjacobs/kube-ops-view.git
+cd kube-ops-view
+
+cat << EOF > deploy/service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    application: kube-ops-view
+    component: frontend
+  name: kube-ops-view
+  annotations:
+    service.beta.kubernetes.io/aws-load-balancer-type: external
+    service.beta.kubernetes.io/aws-load-balancer-scheme: internet-facing
+    service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: instance
+spec:
+  selector:
+    application: kube-ops-view
+    component: frontend
+  type: LoadBalancer
+  loadBalancerClass: service.k8s.aws/nlb
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 8080
+EOF
+
+# built-in kustomization command with -k
+kubectl apply -k deploy
+
+export KOV_POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=kube-ops-view,app.kubernetes.io/instance=kube-ops-view" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $KOV_POD_NAME 8080:8080
+
+# preview > preview running application
