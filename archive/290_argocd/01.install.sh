890bb9eb2496d6d8f023c4142e28b722ea988c56
Dobal <dokeunoh@gmail.com>
2024-05-06 20:47:20 +0900

    Update ArgoCD version

--- archive/290_argocd/01.install.sh
+++ archive/290_argocd/01.install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+export ARGOCD_VER=2.10.9
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v${ARGOCD_VER}/manifests/install.yaml
 
-sudo curl --silent --location -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.4.7/argocd-linux-amd64
+sudo curl --silent --location -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v{ARGOCD_VER}/argocd-linux-amd64
 
 sudo chmod +x /usr/local/bin/argocd
