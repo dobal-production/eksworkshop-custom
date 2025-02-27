5e04b96a1dea8dd0c1dba7f229299dc522ef4328
Dobal <dokeunoh@gmail.com>
2025-02-27 22:28:55 +0900

    Update 00.make-role.sh

--- introduction/setup/00.make-role.sh
+++ introduction/setup/00.make-role.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-export CLUSTER_NAME=eks-workshop
+export EKS_CLUSTER_NAME=eks-workshop
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
@@ -10,7 +10,7 @@ export CLOUD9_ROLE_PROFILE=cloud9-role-profile
 
 echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
 echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
-echo "export CLUSTER_NAME=${CLUSTER_NAME}" | tee -a ~/.bash_profile
+echo "export EKS_CLUSTER_NAME=${EKS_CLUSTER_NAME}" | tee -a ~/.bash_profile
 echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
 
 cat << EOF > cloud9-role.json
