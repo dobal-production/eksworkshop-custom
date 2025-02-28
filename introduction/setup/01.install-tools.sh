3faaac30fd238a53871cc2df03d4a7b26e27aa1b
Dobal <dokeunoh@gmail.com>
2025-02-28 14:53:04 +0900

    Update 01.install-tools.sh

--- introduction/setup/01.install-tools.sh
+++ introduction/setup/01.install-tools.sh
@@ -44,4 +44,9 @@ eksctl completion bash >> ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
+# insatall helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 cd ~/environment/eksworkshop-custom/introduction/setup
